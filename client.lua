edc59c45a7ffdd12203a4a3841a38b7b6e120316
ToastinYou <toastinyou@gmx.com>
2017-11-16 06:27:27 -0500

    Fixed it being toggled while in vehicle.

--- client.lua
+++ client.lua
@@ -4,19 +4,17 @@ Citizen.CreateThread(function()
 		Citizen.Wait(0)
 
     	local ped = GetPlayerPed(-1)
+    	
+    	if DoesEntityExist(ped) and not IsEntityDead(ped) and not IsPauseMenuActive() and not IsPedInAnyVehicle(ped, true) and IsControlJustReleased(0, 19) then
+    		RequestAnimSet("move_ped_crouched")
 
-    	if DoesEntityExist(ped) and not IsEntityDead(ped) and not IsPauseMenuActive() then
-	    	if IsControlJustReleased(0, 19) then
-	    		RequestAnimSet("move_ped_crouched")
-
-	    		if crouched then
-	    			ResetPedMovementClipset(ped, 0.0)
-	    			crouched = false
-	    		elseif not crouched then
-	    			SetPedMovementClipset(ped, "move_ped_crouched", 1.0)
-	    			crouched = true
-	    		end
-	    	end
+    		if crouched then
+    			ResetPedMovementClipset(ped, 0.0)
+    			crouched = false
+    		elseif not crouched then
+    			SetPedMovementClipset(ped, "move_ped_crouched", 1.0)
+    			crouched = true
+    		end
     	end
     end
 end)
